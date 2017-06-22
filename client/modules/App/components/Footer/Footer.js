540790698c17c99ce408c375cd8600ebbd41633b
Mehdi Sadeghi <mehdi.sadeghi@iwm.fraunhofer.de>
2017-06-22 10:52:07 +0200

    Change funding info

--- client/modules/App/components/Footer/Footer.js
+++ client/modules/App/components/Footer/Footer.js
@@ -10,7 +10,7 @@ export function Footer() {
   return (
     <div style={{ background: `#FFF url(${bg}) center` }} className={styles.footer}>
       <p>&copy; European Materials Modelling council CSA (EMMC-CSA)</p>
-      <p>The EMMC-CSA project receives funding from the European Research Council under the European Union's H2020 Programme grant agreement n° 723867</p>
+      <p>The EMMC-CSA project receives funding European Union's H2020 Programme grant agreement n° 723867</p>
     </div>
   );
 }
