57be3a52438b15837b0185e158bed6827eed5a52
Mehdi Sadeghi <mehdi.sadeghi@iwm.fraunhofer.de>
2017-09-05 18:43:24 +0200

    Hide forms on title click

--- client/src/App/components/Header.js
+++ client/src/App/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
 import { Link } from 'react-router'
-import { browserHistory } from "react-router"
 
 import './Header.css'
 import logo from './emmc-x250.png'
@@ -14,7 +13,7 @@ export default function Header(props, context) {
 
         <h1 className='site-title'>
           <img src={logo} alt='logo' className='site-logo' />
-          <Link to="/" onClick={() => {browserHistory.push('/')}}>MODA Portal</Link>
+          <Link to="/" onClick={props.hideForms}>MODA Portal</Link>
           <span className='site-subtitle'> (preview version)</span>
         </h1>
 
